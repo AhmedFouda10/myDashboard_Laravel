9c62c9e7bab6db788d56e2eb9b05435ec0a3a1e4
Ahmed Fouda <58745303+AhmedFouda10@users.noreply.github.com>
2023-01-06 01:40:43 +0200

    second commit

--- resources/views/empty.blade.php
+++ resources/views/empty.blade.php
@@ -2,3 +2,20 @@
 @section('title')
 Empty - فارغه
 @endsection
+
+
+@section('content-title')
+Empty
+@endsection
+
+@section('content-description')
+Empty Description
+@endsection
+
+@section('page-title')
+Empty
+@endsection
+
+@section('content')
+
+@endsection
